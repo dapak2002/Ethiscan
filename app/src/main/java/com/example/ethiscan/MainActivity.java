8635742540c48c91c8cd0e24a3851649acb92af9
dapak2002 <dapak@chapman.edu>
2022-04-09 02:19:58 -0700

    Fixed Import Issue

--- app/src/main/java/com/example/ethiscan/MainActivity.java
+++ app/src/main/java/com/example/ethiscan/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
